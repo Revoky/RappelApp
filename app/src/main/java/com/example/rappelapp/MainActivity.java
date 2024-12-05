c2386f826e734cc15df37d3b412eb69b424cc7b9
GagaYaba <gaelle.lanic@yahoo.fr>
2024-12-05 12:19:12 +0100

    FileManager: manage alarm history; PreferencesManager: save user preferences alarm tone

--- app/src/main/java/com/example/rappelapp/MainActivity.java
+++ app/src/main/java/com/example/rappelapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rappelapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // PreferencesManager
+        PreferencesManager preferencesManager = new PreferencesManager(this);
+
+        // Enregistrer sonnerie
+        String currentTone = preferencesManager.getAlarmTone();
+        if ("default_tone".equals(currentTone)) {
+            preferencesManager.setAlarmTone("new_alarm_tone");
+            currentTone = preferencesManager.getAlarmTone();
+        }
+
+        // MaJ interface utilisateur
+        TextView alarmToneTextView = findViewById(R.id.alarm_tone_text);
+        alarmToneTextView.setText("Sonnerie actuelle : " + currentTone);
     }
 }
---
+++ app/src/main/java/com/example/rappelapp/PreferencesManager.java
@@ -0,0 +1,25 @@
+package com.example.rappelapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferencesManager {
+    private static final String PREF_NAME = "Preferences";
+    private static final String KEY_ALARM_TONE = "alarm_tone";
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    public PreferencesManager(Context context) {
+        sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void setAlarmTone(String tone) {
+        editor.putString(KEY_ALARM_TONE, tone);
+        editor.apply();
+    }
+
+    public String getAlarmTone() {
+        return sharedPreferences.getString(KEY_ALARM_TONE, "default_tone");
+    }
+}
