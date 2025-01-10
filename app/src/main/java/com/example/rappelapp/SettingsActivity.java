c79d6a0fd54c1a933a279a548079e398226705a9
GagaYaba <gaelle.lanic@yahoo.fr>
2025-01-10 19:25:04 +0100

    fix: merge conflicts

--- app/src/main/java/com/example/rappelapp/SettingsActivity.java
+++ app/src/main/java/com/example/rappelapp/SettingsActivity.java
@@ -21,7 +21,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_tones);
 
         preferencesManager = new PreferencesManager(this);
 
